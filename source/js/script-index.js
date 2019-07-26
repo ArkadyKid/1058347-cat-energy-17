5dac248954274eb497ff452727eeb56834de415a
ArkadyKid <kidanov.a.y1986@gmail.com>
2019-07-26 13:32:58 +0300

    Правки

---
+++ source/js/script-index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function () {
+  var navJsClass = 'nav__menu--js';
+  var navMenuElement = document.querySelector('.nav__menu');
+  var toggleMenuElement = document.querySelector('.nav__toggle');
+
+  // navMenuElement.classList.add(navJsClass);
+
+
+  // toggleMenuElement.addEventListener('click', function (evt) {
+  //   evt.preventDefault();
+  //   console.log("Привет");
+  //   navMenuElement.classList.add(navJsClass);
+  //   if (toggleMenuElement.classList.contains(navJsClass)) {
+  //     toggleMenuElement.classList.add()
+  //   };
+  // });
+})();
