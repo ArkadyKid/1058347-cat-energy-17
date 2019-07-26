7a8cd4051e1c04db097e872198ddca9e9c9d2e14
ArkadyKid <kidanov.a.y1986@gmail.com>
2019-07-26 19:24:37 +0300

    Начал реализовать js меню

--- source/js/script-index.js
+++ source/js/script-index.js
@@ -2,18 +2,27 @@
 
 (function () {
   var navJsClass = 'nav__menu--js';
+  var toggleMenuCloseClass = 'nav__toggle--close';
+  var toggleCloseIconClass = 'nav__toggle-icon--close';
+
   var navMenuElement = document.querySelector('.nav__menu');
   var toggleMenuElement = document.querySelector('.nav__toggle');
+  var toggleIconElement = document.querySelector('.nav__toggle-icon');
 
-  // navMenuElement.classList.add(navJsClass);
+  navMenuElement.classList.add(navJsClass);
 
+  toggleMenuElement.addEventListener('click', function (evt) {
+    evt.preventDefault();
 
-  // toggleMenuElement.addEventListener('click', function (evt) {
-  //   evt.preventDefault();
-  //   console.log("Привет");
-  //   navMenuElement.classList.add(navJsClass);
-  //   if (toggleMenuElement.classList.contains(navJsClass)) {
-  //     toggleMenuElement.classList.add()
-  //   };
-  // });
+    if (navMenuElement.classList.contains(navJsClass)) {
+      navMenuElement.classList.remove(navJsClass);
+      toggleMenuElement.classList.add(toggleMenuCloseClass);
+      toggleIconElement.classList.add(toggleCloseIconClass);
+    } else {
+      navMenuElement.classList.add(navJsClass);
+      toggleMenuElement.classList.remove(toggleMenuCloseClass);
+      toggleIconElement.classList.remove(toggleCloseIconClass);
+    }
+  });
+
 })();
