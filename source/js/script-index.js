253f078f49e3987653acbce0643f00b60e4b23ac
ArkadyKid <kidanov.a.y1986@gmail.com>
2019-07-28 13:35:52 +0300

    Закончил главное меню (на некоторое время отключил js меню для коректировки сайта под pp), внёс правки для идентичного отображения в ie11

--- source/js/script-index.js
+++ source/js/script-index.js
@@ -2,6 +2,7 @@
 
 (function () {
   var navJsClass = 'nav__menu--js';
+  var navMenuOpenClass = 'nav__menu--js--open';
   var toggleMenuCloseClass = 'nav__toggle--close';
   var toggleCloseIconClass = 'nav__toggle-icon--close';
 
@@ -10,18 +11,22 @@
   var toggleIconElement = document.querySelector('.nav__toggle-icon');
 
   navMenuElement.classList.add(navJsClass);
+  toggleMenuElement.classList.add(toggleMenuCloseClass);
+  toggleIconElement.classList.add(toggleCloseIconClass);
 
   toggleMenuElement.addEventListener('click', function (evt) {
     evt.preventDefault();
 
     if (navMenuElement.classList.contains(navJsClass)) {
       navMenuElement.classList.remove(navJsClass);
-      toggleMenuElement.classList.add(toggleMenuCloseClass);
-      toggleIconElement.classList.add(toggleCloseIconClass);
-    } else {
-      navMenuElement.classList.add(navJsClass);
+      navMenuElement.classList.add(navMenuOpenClass);
       toggleMenuElement.classList.remove(toggleMenuCloseClass);
       toggleIconElement.classList.remove(toggleCloseIconClass);
+    } else {
+      navMenuElement.classList.add(navJsClass);
+      navMenuElement.classList.remove(navMenuOpenClass);
+      toggleMenuElement.classList.add(toggleMenuCloseClass);
+      toggleIconElement.classList.add(toggleCloseIconClass);
     }
   });
 
