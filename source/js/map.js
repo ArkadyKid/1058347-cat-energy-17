dcfd09d93e4f4889affb490683761d4c869e7377
ArkadyKid <kidanov.a.y1986@gmail.com>
2019-07-31 23:22:39 +0300

    Правки

--- source/js/map.js
+++ source/js/map.js
@@ -10,16 +10,10 @@
 
     var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
       iconLayout: 'default#image',
-      iconImageHref: '../img/map-pin.png',
+      iconImageHref: './img/map-pin.png',
       iconImageSize: [126, 106],
       iconImageOffset: [-3, -42]
     });
-
-    if( window.innerWidth >= 600 ){
-
-    } else {
-      //не выполнять
-    }
 
     myMap.geoObjects
       .add(myPlacemark)
